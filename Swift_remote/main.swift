68d24b1a0274d92db1fdeba62e18e28308ecacfd
Alvaro Alva <7mesino.97@gmail.com>
2020-08-24 14:32:07 -0400

    Initial Commit

---
+++ Swift_remote/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_remote
+//
+//  Created by Alvaro Alva on 8/24/20.
+//  Copyright © 2020 Alvaro Alva. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
